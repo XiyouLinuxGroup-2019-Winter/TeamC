23b10e2312f5abf8959ea3a453b62e8288f915cc
hchc32 <1844382037@qq.com>
2020-03-26 12:55:54 +0800

    1

--- hc/pratice/Linux_c/test.c
+++ hc/pratice/Linux_c/test.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
-
+#include<stdlib.h>
+#include<string.h>
+char *name[10];
 int main()
 {
-
-
+    const char *p = "asdas";
+    name[0] = (char *)malloc(sizeof(char)*10);
+    strcpy(name[0],p);
+    printf("%s\n",name[0]);
+    free(name[0]);
+    printf("%s\n",name[0]);
     return 0;
 }
 
